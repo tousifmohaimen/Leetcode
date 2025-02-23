5dba1056859a7c966679156368e2705002f7fbe3
Tousif Mohaimen <mohaimentousif10@gmail.com>
2025-02-23 18:15:34 +0600

    towsum bruteforce solution

---
+++ problem/1. Two_Sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+//SOLUTION 1 = BRUTEFORCE SOLUTION - o(n^2)
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++){
+        for(let j=i + 1 ; j <nums.length; j++){
+            if(nums[j] === target - nums[i]){
+                return [i,j]
+            }
+        }
+    }
+};
