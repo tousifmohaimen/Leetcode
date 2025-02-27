056893b1e186c64afe4ff57d565ccade744f255d
Tousif Mohaimen <mohaimentousif10@gmail.com>
2025-02-27 21:22:58 +0600

    optimized solution

--- problem/array/Buy_and_Sell_121.js
+++ problem/array/Buy_and_Sell_121.js
@@ -1,4 +1,7 @@
 /* My first Solution
+time complexity: O(n)
+space complexity: O(n)
+
 
 var maxProfit = function(prices) {
     let minPrice = Math.min(...prices);
@@ -12,4 +15,17 @@ var maxProfit = function(prices) {
 };
 
 */
+
+
 //More optimized solution
+
+//time complexity: O(n),space complexity: O(1);
+var maxProfit = function(prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+    for(let i= 0;i < prices.length;i++){
+        minPrice = Math.min(prices[i],minPrice);//1
+        maxProfit = Math.max(maxProfit,prices[i] - minPrice); //5
+    }
+    return maxProfit;
+};
