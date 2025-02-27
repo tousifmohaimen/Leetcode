aad8381ea3b7022d0a581ea78f59804a421fcbb0
Tousif Mohaimen <mohaimentousif10@gmail.com>
2025-02-28 01:34:59 +0600

    more optimized solution with sorting 217

--- problem/array/contains-duplicate_217.js
+++ problem/array/contains-duplicate_217.js
@@ -3,12 +3,25 @@
 //space complexity: O(1)
 
 
+// var containsDuplicate = function(nums) {
+//     for(let i = 0;i < nums.length;i++){
+//         for(let j = i + 1;j <nums.length;j++){
+//             if(nums[i] === nums[j])
+//                 return true;
+//         }
+//     }
+//     return false
+// };
+
+//more optimized solution
+//time complexity: O(n log n) if we consider the sort function
+//space complexity: O(1)
+
 var containsDuplicate = function(nums) {
-    for(let i = 0;i < nums.length;i++){
-        for(let j = i + 1;j <nums.length;j++){
-            if(nums[i] === nums[j])
-                return true;
-        }
+    nums.sort((a,b) => a - b);
+    for(let i = 0;i < nums.length - 1;i++){
+        if(nums[i] === nums[i + 1])
+            return true;
     }
     return false
-};
+}
