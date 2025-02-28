5d161617fac40bc415db54baa6504ab0aba7e806
Tousif Mohaimen <mohaimentousif10@gmail.com>
2025-03-01 01:17:36 +0600

    optimized with hash map solution with o(n) for two sum

--- problem/array/1. Two_Sum.js
+++ problem/array/1. Two_Sum.js
@@ -13,3 +13,15 @@ var twoSum = function(nums, target) {
         }
     }
 };
+
+//SOLUTION 2 = USING HASHMAP - o(n) => most optimized solution
+var twoSum = function(nums, target) {
+    let map = new Map();
+    for(let i =0;i < nums.length;i++){
+        let temp = target - nums[i];
+        if(map.has(temp)){
+            return [map.get(temp),i]
+        }
+        map.set(nums[i],i);
+    }
+}
