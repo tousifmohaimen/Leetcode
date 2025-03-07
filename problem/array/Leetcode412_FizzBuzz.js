f189063ba9350b32f22cb08c6f2a87736f8028aa
Tousif Mohaimen <mohaimentousif10@gmail.com>
2025-03-07 22:25:03 +0600

    leetcode 412

---
+++ problem/array/Leetcode412_FizzBuzz.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    answer = [];
+    for(let i = 1;i <= n;i++){
+        if(i % 3 === 0 && i % 5 === 0)
+            answer.push("FizzBuzz");
+        else if(i % 3 == 0)
+            answer.push("Fizz");
+        else if(i % 5 === 0)
+            answer.push("Buzz");
+        else{
+            answer.push(i.toString());
+        }
+    }
+  return answer
+};
+
+
+//time complexity: O(n)
+//space complexity: O(n)
